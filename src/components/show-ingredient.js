937e7d36d6f3ac08907e23af0204845aa8e9f732
Miguel <miguel@redradix.com>
2016-02-18 19:49:52 +0100

    cutre fix to enable history back on show ingredient

--- src/components/show-ingredient.js
+++ src/components/show-ingredient.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router'
 import { translate } from 'react-i18next/lib'
 
 class ShowIngredient extends Component {
+  shouldComponentUpdate(props) {
+    return !!props.ingredient 
+  }
   render() {
     const { ingredient, ingredient: {id, name, cost, stock }, removeIngredient, t } = this.props
     return (
