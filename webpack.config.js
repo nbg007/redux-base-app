acd988f7edbd7ba98c1b812c3db1bf711de6b9cc
Miguel <miguel@redradix.com>
2016-02-19 18:16:40 +0100

    refactor naming and test passing

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  //devtool: 'source-map',
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
