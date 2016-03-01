4525ccb99eb884a240726948020c5841d5219a8e
Sara Moreno <sara.morenoj@gmail.com>
2016-03-01 17:16:12 +0100

    styling home buttons

--- src/components/common/home.js
+++ src/components/common/home.js
@@ -6,17 +6,19 @@ export function Home({t}) {
   return (
     <div>
       <h3>{t('home.title')}</h3>
-      <p><Link to="/ingredients">{t('home.ingredientsLink')}</Link>
-      {' '}
-      <Link to="/ingredients/create">{t('home.createIngredientLink')}</Link></p>
+      <div className='button-wrap'>
+        <p><Link className='button button-primary' to="/ingredients">{t('home.ingredientsLink')}</Link>
+        {' '}
+        <Link className='button button-primary' to="/ingredients/create">{t('home.createIngredientLink')}</Link></p>
 
-      <p><Link to="/dishes">{t('home.dishesLink')}</Link>
-      {' '}
-      <Link to="/dishes/create">{t('home.createDishLink')}</Link></p>
+        <p><Link className='button button-primary' to="/dishes">{t('home.dishesLink')}</Link>
+        {' '}
+        <Link className='button button-primary' to="/dishes/create">{t('home.createDishLink')}</Link></p>
 
-      <p><Link to="/orders">{t('home.ordersLink')}</Link>
-      {' '}
-      <Link to="/orders/create">{t('home.createOrderLink')}</Link></p>
+        <p><Link className='button button-primary' to="/orders">{t('home.ordersLink')}</Link>
+        {' '}
+        <Link className='button button-primary' to="/orders/create">{t('home.createOrderLink')}</Link></p>
+      </div>
     </div>
   );
 }
